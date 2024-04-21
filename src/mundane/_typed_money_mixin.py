7c025c5e1b10753c7c15f05d2d970dab58cc3870
Linus Lewandowski <linus@lew21.net>
2024-04-22 00:58:06 +0200

    Move overloads from AnyMoney to a separate mixin.

---
+++ src/mundane/_typed_money_mixin.py
@@ -0,0 +1,113 @@
+from decimal import Decimal as decimal
+from types import NotImplementedType
+from typing import Self, overload
+
+from ._money import OverloadPaddingType1, OverloadPaddingType2
+
+
+class TypedMoneyMixin:
+
+	@overload
+	def __lt__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __lt__(self, other: OverloadPaddingType2) -> NotImplementedType:
+		...
+
+	@overload
+	def __lt__(self, other: Self) -> bool:
+		...
+
+	def __lt__(self, other: object) -> bool | NotImplementedType:
+		...
+
+	@overload
+	def __le__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __le__(self, other: OverloadPaddingType2) -> NotImplementedType:
+		...
+
+	@overload
+	def __le__(self, other: Self) -> bool:
+		...
+
+	def __le__(self, other: object) -> bool | NotImplementedType:
+		...
+
+	@overload
+	def __gt__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __gt__(self, other: OverloadPaddingType2) -> NotImplementedType:
+		...
+
+	@overload
+	def __gt__(self, other: Self) -> bool:
+		...
+
+	def __gt__(self, other: object) -> bool | NotImplementedType:
+		...
+
+	@overload
+	def __ge__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __ge__(self, other: OverloadPaddingType2) -> NotImplementedType:
+		...
+
+	@overload
+	def __ge__(self, other: Self) -> bool:
+		...
+
+	def __ge__(self, other: object) -> bool | NotImplementedType:
+		...
+
+	@overload
+	def __add__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __add__(self, other: OverloadPaddingType2) -> NotImplementedType:
+		...
+
+	@overload
+	def __add__(self, other: Self) -> Self:
+		...
+
+	def __add__(self, other: object) -> Self | NotImplementedType:
+		...
+
+	@overload
+	def __sub__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __sub__(self, other: OverloadPaddingType2) -> NotImplementedType:
+		...
+
+	@overload
+	def __sub__(self, other: Self) -> Self:
+		...
+
+	def __sub__(self, other: object) -> Self | NotImplementedType:
+		...
+
+	@overload
+	def __truediv__(self, other: OverloadPaddingType1) -> NotImplementedType:
+		...
+
+	@overload
+	def __truediv__(self, other: int | decimal) -> Self:
+		...
+
+	@overload
+	def __truediv__(self, other: Self) -> decimal:
+		...
+
+	def __truediv__(self, other: object) -> decimal | Self | NotImplementedType:
+		...
